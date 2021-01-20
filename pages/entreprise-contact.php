eec54aa69e38e75dc7b6f2ea7dbc53144ffae3da
benbegins <benoit.beghyn@gmail.com>
2021-01-20 18:33:25 +0100

    tel searchprotech

--- pages/entreprise-contact.php
+++ pages/entreprise-contact.php
@@ -27,15 +27,17 @@ get_header('entreprise');
                         <p class="uppercase text-gray text-xs mb-2">Adresse</p>
                         <p>164 rue de Vaugirard <br>75015 PARIS</p>
                         <a href="https://goo.gl/maps/vTF9nJBD1Qoz6Bi36" class="btn-secondary btn-dark" target="_blank">Voir le plan</a>
+                        <div class="mt-4">
+                            <a href="https://www.facebook.com/exos.recrutement/" class="icon-social icon-facebook-dark no-transition" target="_blank">Facebook</a>
+                            <a href="https://www.linkedin.com/company/2740192/admin/" class="icon-social icon-linkedin-dark ml-4 no-transition" target="_blank">Linkedin</a>
+                        </div>
                     </div>
                     <div class="mt-10 md:w-2/5 md:ml-auto">
                         <p class="uppercase text-gray text-xs mb-2">Téléphone & Fax</p>
                         <p>t. 01 40 19 72 75</p>
                         <p>f. 01 40 19 72 82</p>
-                        <div class="mt-4">
-                            <a href="https://www.facebook.com/exos.recrutement/" class="icon-social icon-facebook-dark no-transition" target="_blank">Facebook</a>
-                            <a href="https://www.linkedin.com/company/2740192/admin/" class="icon-social icon-linkedin-dark ml-4 no-transition" target="_blank">Linkedin</a>
-                        </div>
+                        <p class="mt-4"><strong>SearchProTech</strong></p>
+                        <p>t. 01 40 19 72 79</p>
                     </div>
                 </div>
             </div>
